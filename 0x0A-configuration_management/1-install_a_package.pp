f704a97c602fd259a6622368215c3481e49ccef0
Aed10 <eddyyanyayoub@gmail.com>
2024-02-23 20:25:17 +0100

    Update puppet code to install flask version 2.1.0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,8 @@
 # Using Puppet, install flask from pip3.
 
-exec {'istall_flask':
-  command  => '/usr/bin/pip3 install flask==2.1.0',
+package {'istall_flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 
 
 }
