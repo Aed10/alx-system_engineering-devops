c60cc27e1b3364221de3261e9e76b51a3f1e286d
Aed10 <eddyyanyayoub@gmail.com>
2024-02-23 21:00:47 +0100

    Install puppet-lint gem version 2.1.1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-#  install flask from pip3.
-
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-
+# installing puppet link
+package { 'puppet-lint':
+    ensure   => '2.1.1',
+    provider => 'gem',
 }
