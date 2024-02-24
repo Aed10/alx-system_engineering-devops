0842e51de5db81a14986a73a69e8acc4b904d0eb
Aed10 <eddyyanyayoub@gmail.com>
2024-02-24 19:09:49 +0100

    Update exec command for installing Flask and Werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,8 +4,7 @@ package { 'pip3':
 }
 
 exec { 'install_flask_and_werkzeug':
-  command     => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
-  path        => ['/usr/bin', '/usr/local/bin'],
-  environment => 'PATH=/usr/bin:/usr/local/bin',
-  unless      => '/usr/bin/pip3 freeze | grep -q "Flask==2.1.0" && /usr/bin/pip3 freeze | grep -q "Werkzeug==2.1.1"',
+  command => 'pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 freeze | grep -q "Flask==2.1.0" && /usr/bin/pip3 freeze | grep -q "Werkzeug==2.1.1"',
 }
