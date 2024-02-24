789d89e31dbeaee2ea043d4e4c714298f8b9b9ef
Aed10 <eddyyanyayoub@gmail.com>
2024-02-24 18:36:10 +0100

    Install Flask and Werkzeug via pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# installing Flask package
-package { 'install flask':
-    ensure   => '2.1.0',
-    name     => 'flask',
-    provider => 'pip3',
+# Ensure Flask and Werkzeug are installed via pip3
+package { 'pip3':
+  ensure   => installed,
+}
+
+exec { 'install_flask_and_werkzeug':
+  command     => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  path        => ['/usr/bin', '/usr/local/bin'],
+  environment => 'PATH=/usr/bin:/usr/local/bin',
+  unless      => '/usr/bin/pip3 freeze | grep -q "Flask==2.1.0" && /usr/bin/pip3 freeze | grep -q "Werkzeug==2.1.1"',
 }
