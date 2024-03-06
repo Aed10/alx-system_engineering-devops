7905aa174f3f253d4f18bbd5b9461ddeb67d1b9e
Aed10 <eddyyanyayoub@gmail.com>
2024-03-06 13:50:09 +0100

    Add exec for apt update and restart nginx command

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,9 +3,14 @@
 #   The value of the custom HTTP header must be the hostname of the server Nginx is running on
 #   The custom HTTP header must be added to the response of the default Nginx virtual host
 
+exec { 'apt-update':
+  command  => 'apt -y apt update && apt -y apt upgrade',
+  provider => 'shell',
+}
 # Ensure Nginx package is installed
 package { 'nginx':
-  ensure  => present,
+  ensure   => installed,
+  provider => 'apt',
 }
 
 # Add custom HTTP header to Nginx configuration
@@ -17,3 +22,7 @@ file_line { 'add_custom_header':
   path   => '/etc/nginx/nginx.conf',
 }
 
+exec { 'restart_nginx':
+  command     => 'systemctl restart nginx',
+  refreshonly => true,
+}
