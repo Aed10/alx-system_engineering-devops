c03c549b5cf0273e466851c88a172ecf3eccdc29
Aed10 <eddyyanyayoub@gmail.com>
2024-03-06 14:03:25 +0100

    Create custom HTTP header response with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,33 +1,8 @@
-# A puppet manifest that automate the creating of a custom HTTP header response
-#   The name of the custom HTTP header must be X-Served-By
-#   The value of the custom HTTP header must be the hostname of the server Nginx is running on
-#   The custom HTTP header must be added to the response of the default Nginx virtual host
-
-exec { 'apt-update':
-  command  => 'apt -y apt update && apt -y apt upgrade',
-  provider => 'shell',
-}
-# Ensure Nginx package is installed
-package { 'nginx':
-  ensure   => installed,
-  provider => 'apt',
-  before   => File['/etc/nginx/sites-available/default'],
-}
-
-# Add custom HTTP header to Nginx configuration
-file_line { 'add_custom_header':
-  ensure => present,
-  line   => "add_header X-Served-By ${hostname};",
-  match  => '^http {',
-  after  => '^http {',
-  path   => '/etc/nginx/sites-available/default',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
-
-exec { 'restart_nginx':
-  command  => 'service nginx restart',
-  provider => 'shell',
+# Automation: creates a custom HTTP header response with Puppet.
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
