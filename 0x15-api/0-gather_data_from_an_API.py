d7c957e78052045e49f3018367caa87ba6f1d4a9
Aed10 <eddyyanyayoub@gmail.com>
2024-05-01 21:24:44 +0100

    Add script to gather data from an API and update README.md

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress. """
+import requests
+import sys
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+    response = requests.get(f"{url}/users/{id}").json()
+    name = response.get("name")
+    data = requests.get(f"{url}/todos?userId={id}").json()
+
+    total_tasks = len(data)
+    completed = [task for task in data if task.get("completed")]
+    print(
+        f"Employee {name} is done with tasks({len(completed)}/{total_tasks})"
+        if name
+        else "No employee found for the ID"
+    )
+    for task in completed:
+        print("\t {}".format(task.get("title")))
