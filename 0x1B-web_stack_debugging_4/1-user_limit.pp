9a1a066bf9dd84d6afc8d96452eacaaa24313b23
Aed10 <eddyyanyayoub@gmail.com>
2024-08-23 03:03:07 +0100

    Update file limits for 'holberton' user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,13 @@
-# Reconfigure the OS for 'holberton' to login and open a file without any error message
+# Ensure that the 'holberton' user has sufficient file limits
 
+# Add or modify the hard file limit for the 'holberton' user
 exec { 'increase-hard-file-limit-holberton-user':
-  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+  command => 'sed -i "/^holberton hard/ {s/^[^ ]* [^ ]* /holberton hard nofile /; s/[0-9][0-9]* /50000 /}" /etc/security/limits.conf',
+  path    => '/usr/bin/',  # Assuming sed is in /usr/bin/
 }
 
+# Add or modify the soft file limit for the 'holberton' user
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+  command => 'sed -i "/^holberton soft/ {s/^[^ ]* [^ ]* /holberton soft nofile /; s/[0-9][0-9]* /50000 /}" /etc/security/limits.conf',
+  path    => '/usr/bin/',  # Assuming sed is in /usr/bin/
 }
